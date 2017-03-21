e61a924282795ff70cf95aa5209f348520f140a6
mike stedman <github@ravuya.com>
2017-03-21 09:59:52 -0600

    Print commits, though the inclusion range seems wrong.

--- Changelog/Program.fs
+++ Changelog/Program.fs
@@ -1,10 +1,10 @@
 ﻿open LibGit2Sharp
 
-let getCommitsSinceTag (repo : Repository) (tag : Tag) =
+let commitsSinceTag (tag : Tag) (repo : Repository) =
     let filter = new CommitFilter (IncludeReachableFrom = tag)
     repo.Commits.QueryBy(filter)
 
-let findLatestTag (repo : LibGit2Sharp.Repository) =
+let latestTag (repo : Repository) =
     repo.Tags |> Seq.cast<Tag> 
     |> Seq.maxBy(fun t -> 
         let commit : Commit = downcast t.Target
@@ -13,8 +13,9 @@ let findLatestTag (repo : LibGit2Sharp.Repository) =
 
 [<EntryPoint>]
 let main argv = 
-    let repo = new LibGit2Sharp.Repository(@"C:\code\alpha-tracker")
-    let lastTag = findLatestTag repo
-    let commitsSinceTag = getCommitsSinceTag repo lastTag |> Seq.cast<Commit>
+    let repo = new Repository(@"C:\code\alpha-tracker")
+    let lastTag = latestTag repo
+    let commitsSinceTag = commitsSinceTag lastTag repo |> Seq.cast<Commit>
     printfn "%i commits since tag %s" (Seq.length commitsSinceTag) lastTag.FriendlyName
+    Seq.iter (printf "%s\n") (commitsSinceTag |> Seq.map(fun c -> c.MessageShort))
     0 // return an integer exit code
