c495626b7c2b054d0c7a27b5ecbbd4418c891924
mike stedman <github@ravuya.com>
2017-03-21 09:54:57 -0600

    Pulled list of tags, got most recent tag, got commit range (though that seems to be broken)

--- Changelog/Program.fs
+++ Changelog/Program.fs
@@ -1,7 +1,20 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open LibGit2Sharp
+
+let getCommitsSinceTag (repo : Repository) (tag : Tag) =
+    let filter = new CommitFilter (IncludeReachableFrom = tag)
+    repo.Commits.QueryBy(filter)
+
+let findLatestTag (repo : LibGit2Sharp.Repository) =
+    repo.Tags |> Seq.cast<Tag> 
+    |> Seq.maxBy(fun t -> 
+        let commit : Commit = downcast t.Target
+        commit.Committer.When
+    ) // TODO: This is probably not the best way to do it?
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let repo = new LibGit2Sharp.Repository(@"C:\code\alpha-tracker")
+    let lastTag = findLatestTag repo
+    let commitsSinceTag = getCommitsSinceTag repo lastTag |> Seq.cast<Commit>
+    printfn "%i commits since tag %s" (Seq.length commitsSinceTag) lastTag.FriendlyName
     0 // return an integer exit code
