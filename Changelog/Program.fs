bf35cd9059525ad59e3ad67ed6dab987264d5ca0
mike stedman <github@ravuya.com>
2017-03-21 10:13:05 -0600

    Add some command line parsing (very ghetto) and throw errors

--- Changelog/Program.fs
+++ Changelog/Program.fs
@@ -1,4 +1,11 @@
 ﻿open LibGit2Sharp
+open System.IO
+open System
+
+type CommandLineArgs = {
+    // Location of the repo on disk
+    repoPath: string;
+}
 
 let commitsSinceTag (tag : Tag) (repo : Repository) =
     let filter = new CommitFilter (ExcludeReachableFrom = tag, FirstParentOnly = true)
@@ -11,12 +18,26 @@ let latestTag (repo : Repository) =
         commit.Committer.When
     ) // TODO: This is probably not the best way to do it?
 
+let parseArgs argv =
+    // TODO: make a fancier parser later
+    let maybeArgs = 
+        match argv with
+        | [|arg|]   -> { repoPath = arg }
+        | _         -> failwith "Needs path to a checked-out git repository on disk." // TODO: better errors later
+    match Directory.Exists maybeArgs.repoPath with
+        | true  -> maybeArgs
+        | _     -> failwithf "The directory specified (%s) does not exist." maybeArgs.repoPath
+
 [<EntryPoint>]
 let main argv = 
-    let repo = new Repository(@"C:\code\alpha-tracker")
-    let lastTag = latestTag repo
-    let commitsSinceTag = commitsSinceTag lastTag repo |> Seq.cast<Commit>
-    printfn "%i commits since tag %s" (Seq.length commitsSinceTag) lastTag.FriendlyName
-    // Print all the commits to the console. TODO: Make a nice output file so we don't have to pipe.
-    Seq.iter (printf "%s\n") (commitsSinceTag |> Seq.map(fun c -> c.MessageShort))
+    try
+        let config = parseArgs argv
+        let repo = new Repository(config.repoPath)
+        let lastTag = latestTag repo
+        let commitsSinceTag = commitsSinceTag lastTag repo |> Seq.cast<Commit>
+        printfn "%i commits since tag %s" (Seq.length commitsSinceTag) lastTag.FriendlyName
+        // Print all the commits to the console. TODO: Make a nice output file so we don't have to pipe.
+        Seq.iter (printf "%s\n") (commitsSinceTag |> Seq.map(fun c -> c.MessageShort))
+    with
+        | Failure msg -> printfn "Failed: %s" msg; Environment.Exit(1)    
     0
